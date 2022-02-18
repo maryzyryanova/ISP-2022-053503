af01d8a6760dc834db9c7ad3f3955bc34883bc39
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-02-18 10:37:20 +0300

    countWords

--- lab_1.py
+++ lab_1.py
@@ -1,11 +1,21 @@
-def countWords():
-    
+def countWords(arr):
+    dictionary = dict()
+    for i in range(0, len(arr)):
+        counter = 1
+        for j in range(i+1, len(arr)):
+            if(arr[i] == arr[j]):
+                counter += 1
+        dictionary[counter] = arr[i]
+    return dictionary
+
 
 def main():
     str = input()
     print(f"Your string: {str}")
     arr = str.split()
     print(arr)
+    for key, value in countWords(arr).items:
+        prin
 
 main()
 
