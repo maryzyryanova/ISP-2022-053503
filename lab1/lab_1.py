7812491800ad31fb08eb5232d3a4d2d120051f37
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-03-03 23:39:28 +0300

    using types annotations

--- lab1/lab_1.py
+++ lab1/lab_1.py
@@ -1,13 +1,16 @@
 '''Lab_1'''
+import array
 import re
 import os
 from statistics import median
 
-def count_words(input_string):
+from isort import file
+
+def count_words(input_string) -> dict:
     '''Count amount of words in the text'''
-    arr = re.split(r'\, |\. |\; |\! |\? |\... |\ |\!|\.|\?|\...|\,|\;', input_string)
+    arr: array = re.split(r'\, |\. |\; |\! |\? |\... |\ |\!|\.|\?|\...|\,|\;', input_string)
     check_empty(arr)
-    dictionary = {}
+    dictionary: dict = {}
     for arr_element in arr:
         if arr_element != 0 :
             if not dictionary.get(arr_element) :
@@ -16,24 +19,24 @@ def count_words(input_string):
                 dictionary[arr_element] += 1
     return dictionary
 
-def print_dictionary(dictionary):
+def print_dictionary(dictionary) -> None:
     '''Function to print dictionary'''
     for key, value in dictionary.items():
         print(f"{value} - {key}")
 
-def check_empty(arr):
+def check_empty(arr) -> None:
     '''Check string on empty elements'''
     if arr.__contains__('') :
         arr.remove('')
 
-def create_dictionary(number_of_ngrams, input_string):
+def create_dictionary(number_of_ngrams, input_string) -> dict:
     '''Creating and using dictionary'''
-    arr = re.split(r'\, |\. |\; |\! |\? |\... |\ |\!|\.|\?|\...|\,|\;', input_string)
+    arr: array = re.split(r'\, |\. |\; |\! |\? |\... |\ |\!|\.|\?|\...|\,|\;', input_string)
     check_empty(arr)
-    output = "".join(arr)
-    dictionary = {}
+    output: str = "".join(arr)
+    dictionary: dict = {}
     for i in range(0, len(output) - number_of_ngrams + 1, 1):
-        temp = ''
+        temp: str = ''
         for j in range (i, i + number_of_ngrams):
             temp += output[j]
         if temp != '' :
@@ -43,42 +46,42 @@ def create_dictionary(number_of_ngrams, input_string):
                 dictionary[temp] += 1
     return dictionary
 
-def find_top(dictionary, top_k):
+def find_top(dictionary, top_k) -> None:
     '''Finding top K amount of n-grams'''
-    sorted_dictionary = {}
-    sorted_values = sorted(dictionary, key = dictionary.get)
-    counter = 0
+    sorted_dictionary: dict = {}
+    sorted_values: dict = sorted(dictionary, key = dictionary.get)
+    counter: int = 0
     for s_v in sorted_values:
         sorted_dictionary[s_v] = dictionary[s_v]
         counter += 1
     if counter < top_k :
         print_dictionary(sorted_dictionary)
     else:
-        dictionary = dict(reversed(list(sorted_dictionary.items())))
+        dictionary: dict = reversed(list(sorted_dictionary.items()))
         print_top(dictionary, top_k)
 
-def print_top(dictionary, top_k):
+def print_top(dictionary, top_k) -> None:
     '''Function to print top K elements'''
-    k = 0
+    k: int = 0
     for key, value in dictionary.items():
         if k >= top_k :
             break
         print(f"{value} - {key}")
         k += 1
 
-def main():
+def main() -> None:
     '''Main function'''
     try:
-        number_of_ngrams = int(input("Enter N: "))
-        top_k = int(input("Enter K: "))
+        number_of_ngrams: int = int(input("Enter N: "))
+        top_k: int = int(input("Enter K: "))
         with open(r'/lab_1/data/file.txt', encoding = "utf8") as file_to_open:
             if os.path.getsize('/lab_1/data/file.txt') == 0:
                 raise EOFError("File is empty")
-            input_string = file_to_open.read()
-            dictionary = count_words(input_string)
+            input_string: str = file_to_open.read()
+            dictionary: dict = count_words(input_string)
             print(f"\nMedian: {median(dictionary.values())}")
             print(f"Average: {sum(dictionary.values()) / len(dictionary.values())}\n")
-            dictionary = create_dictionary(number_of_ngrams, input_string)
+            dictionary: dict = create_dictionary(number_of_ngrams, input_string)
             find_top(dictionary, top_k)
             file_to_open.close()
 
