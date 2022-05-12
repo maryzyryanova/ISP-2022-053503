7f9cfa9c9ae96f30e069efb616efefc4b2cd3b0b
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-05-13 01:25:22 +0300

    admin registrated

--- web/main/admin.py
+++ web/main/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Bell, Dicipline, Teacher, Student, Schedule, Group
 
-# Register your models here.
+admin.site.register(Bell)
+admin.site.register(Dicipline)
+admin.site.register(Teacher)
+admin.site.register(Student)
+admin.site.register(Schedule)
+admin.site.register(Group)
