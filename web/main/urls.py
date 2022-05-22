5e3f193af9b006bcc19574193c3008af1f884959
maryzyryanova <79420458+maryzyryanova@users.noreply.github.com>
2022-05-22 03:00:22 +0300

    6 hours later

--- web/main/urls.py
+++ web/main/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path, include
-from .views import main
+from .views import ScheduleView, StudentsView, TeachersView, main
 
 urlpatterns = [
     path('', main),
+    path('students/', StudentsView.as_view()),
+    path('teachers/', TeachersView.as_view()),
+    path('schedule/', ScheduleView.as_view()),
 ]
--- web/main/views.py
+++ web/main/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from urllib import request
+from django.shortcuts import redirect, render
 from django.views import View
-from .models import Student
+from .models import Schedule, Student, Teacher
 
 def main(request):
     return render(request, 'main_window.html')
@@ -8,4 +9,16 @@ def main(request):
 class StudentsView(View):
     def get(self, request):
         students = Student.objects.all()
-        return render(request, "students/student_list.html", {"student_list.html": students})
+        return render(request, "students/student_list.html", {"student_list": students})
+
+class TeachersView(View):
+    def get(self, request):
+        teachers = Teacher.objects.all()
+        return render(request, "teachers_list.html", {"teachers_list": teachers})
+
+
+class ScheduleView(View):
+    def get(self, request):
+        schedule = Schedule.objects.all()
+        return render(request, "schedule_list.html", {"schedule_list": schedule})
+
